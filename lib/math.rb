574b338029053db20622a400ab66595e596a0f44
Alec Scully <scullyalec@gmail.com>
2020-08-24 23:32:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,31 +1,25 @@
 def addition(num1, num2)
   sum = num1 + num2
-  puts sum
 end
 
 def subtraction(num1, num2)
   difference = num1 - num2
-  puts difference 
 end
 
 def division(num1, num2)
   quotient = num1 / num2
-  puts quotient
 end
 
 def multiplication(num1, num2)
   product = num1 * num2
-  puts product
 end
 
 def modulo(num1, num2)
   mod = num1 % num2
-  puts mod
 end
 
 def square_root(num)
   Math.sqrt(num)
-  puts Math.sqrt(num)
 end
 
 def testing 
